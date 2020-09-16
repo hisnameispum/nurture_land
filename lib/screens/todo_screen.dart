4353afd03c57a317ab3d9ed92e08a71948553253
hisnameispum <sirasitxp@gmail.com>
2020-09-16 16:51:39 -0700

    Modified bottom navigation bar

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -15,7 +15,7 @@ class _Todo_ScreenState extends State<Todo_Screen> {
     });
   }
 
-  @override
+  @override 
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
@@ -26,35 +26,7 @@ class _Todo_ScreenState extends State<Todo_Screen> {
               child: Expanded(
                 child: Column(
                   children: [
-                    Container(
-                      height: 90,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: [
-                          TreeStory(
-                            treeName: "Tree1",
-                            treeColor: Colors.orange,
-                          ),
-                          TreeStory(
-                            treeName: "Tree2",
-                            treeColor: Colors.red,
-                          ),
-                          TreeStory(
-                            treeName: "Tree3",
-                            treeColor: Colors.blue,
-                          ),
-                          TreeStory(
-                            treeName: "Tree4",
-                            treeColor: Colors.purple,
-                          ),
-                        ],
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border(
-                            bottom: BorderSide(
-                                width: 0.2, color: Colors.greenAccent)),
-                      ),
-                    ),
+                    TopBarStory(),
                     Expanded(
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -113,6 +85,45 @@ class _Todo_ScreenState extends State<Todo_Screen> {
   }
 }
 
+class TopBarStory extends StatelessWidget {
+  const TopBarStory({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 90,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          TreeStory(
+            treeName: "Tree1",
+            treeColor: Colors.orange,
+          ),
+          TreeStory(
+            treeName: "Tree2",
+            treeColor: Colors.red,
+          ),
+          TreeStory(
+            treeName: "Tree3",
+            treeColor: Colors.blue,
+          ),
+          TreeStory(
+            treeName: "Tree4",
+            treeColor: Colors.purple,
+          ),
+        ],
+      ),
+      decoration: BoxDecoration(
+        border: Border(
+            bottom: BorderSide(
+                width: 0.2, color: Colors.greenAccent)),
+      ),
+    );
+  }
+}
+
 class TreeStory extends StatelessWidget {
   final String treeName;
   final Color treeColor;
@@ -132,29 +143,3 @@ class TreeStory extends StatelessWidget {
   }
 }
 
-//class BottomBar extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Container(
-//        child: Row(
-//          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//          children: [
-//            Icon(
-//              Icons.book,
-//              color: Colors.white,
-//            ),
-//            Icon(
-//              Icons.add_circle,
-//              color: Colors.white,
-//              size: 50,
-//            ),
-//            Icon(
-//              Icons.whatshot,
-//              color: Colors.white,
-//            ),
-//          ],
-//        ),
-//        height: 50.0,
-//        color: Color(0xFF00b894));
-//  }
-//}
