f5da9e92cfa522f49c0ede99e96e822f362cd145
hisnameispum <sirasitxp@gmail.com>
2020-09-16 01:45:51 -0700

    Added main content of the page

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Todo_Screen extends StatelessWidget{
+class Todo_Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -18,14 +18,55 @@ class Todo_Screen extends StatelessWidget{
                     child: ListView(
                       scrollDirection: Axis.horizontal,
                       children: [
-                        TreeStory(treeName: "Tree1", treeColor: Colors.orange,),
-                        TreeStory(treeName: "Tree2", treeColor: Colors.red,),
-                        TreeStory(treeName: "Tree3", treeColor: Colors.blue,),
-                        TreeStory(treeName: "Tree4", treeColor: Colors.purple,),
+                        TreeStory(
+                          treeName: "Tree1",
+                          treeColor: Colors.orange,
+                        ),
+                        TreeStory(
+                          treeName: "Tree2",
+                          treeColor: Colors.red,
+                        ),
+                        TreeStory(
+                          treeName: "Tree3",
+                          treeColor: Colors.blue,
+                        ),
+                        TreeStory(
+                          treeName: "Tree4",
+                          treeColor: Colors.purple,
+                        ),
                       ],
                     ),
                     decoration: BoxDecoration(
-                      border: Border(bottom: BorderSide(width: 0.2, color: Colors.greenAccent)),
+                      border: Border(
+                          bottom: BorderSide(
+                              width: 0.2, color: Colors.greenAccent)),
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          '"Some random quote."',
+                          style: TextStyle(fontSize: 30),
+                        ),
+                        CircleAvatar(
+                          backgroundColor: Color(0xFF00b894),
+                          minRadius: 100,
+                        ),
+                        FlatButton(
+                          onPressed: () {
+//                            Start Timer
+                          },
+                          padding:
+                              EdgeInsets.symmetric(vertical: 5, horizontal: 30),
+                          color: Colors.red,
+                          child: Text(
+                            'Start',
+                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
@@ -37,31 +78,28 @@ class Todo_Screen extends StatelessWidget{
       ),
     );
   }
-
-
 }
 
 class TreeStory extends StatelessWidget {
   final String treeName;
   final Color treeColor;
-  TreeStory({@required this.treeName, @required this.treeColor});
 
+  TreeStory({@required this.treeName, @required this.treeColor});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8,horizontal: 5),
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
       child: CircleAvatar(
-          backgroundColor: treeColor,
-          child: Text(treeName),
-          minRadius: 35,
+        backgroundColor: treeColor,
+        child: Text(treeName),
+        minRadius: 35,
       ),
     );
   }
 }
 
 class BottomBar extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -69,8 +107,8 @@ class BottomBar extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Icon(
-                Icons.book,
-                color: Colors.white,
+              Icons.book,
+              color: Colors.white,
             ),
             Icon(
               Icons.add_circle,
