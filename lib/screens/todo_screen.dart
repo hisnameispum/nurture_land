27ada46a15ff4ffacd4184dbce56294964cc08d9
hisnameispum <sirasitxp@gmail.com>
2020-09-16 01:04:17 -0700

    Added the story feature at the top of screen.

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -9,36 +9,81 @@ class Todo_Screen extends StatelessWidget{
       body: Stack(
         alignment: Alignment.bottomCenter,
         children: [
-          ListView(
-            children: [
-
-            ],
-          ),
-          Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          SafeArea(
+            child: Expanded(
+              child: Column(
                 children: [
-                  Icon(
-                      Icons.book,
-                      color: Colors.white,
-                  ),
-                  Icon(
-                    Icons.add_circle,
-                    color: Colors.white,
-                    size: 50,
-                  ),
-                  Icon(
-                    Icons.whatshot,
-                    color: Colors.white,
+                  Container(
+                    height: 90,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        TreeStory(treeName: "Tree1", treeColor: Colors.orange,),
+                        TreeStory(treeName: "Tree2", treeColor: Colors.red,),
+                        TreeStory(treeName: "Tree3", treeColor: Colors.blue,),
+                        TreeStory(treeName: "Tree4", treeColor: Colors.purple,),
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(width: 0.2, color: Colors.greenAccent)),
+                    ),
                   ),
                 ],
               ),
-              height: 50.0,
-              color: Color(0xFF00b894)),
+            ),
+          ),
+          BottomBar(),
         ],
       ),
     );
   }
 
 
+}
+
+class TreeStory extends StatelessWidget {
+  final String treeName;
+  final Color treeColor;
+  TreeStory({@required this.treeName, @required this.treeColor});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8,horizontal: 5),
+      child: CircleAvatar(
+          backgroundColor: treeColor,
+          child: Text(treeName),
+          minRadius: 35,
+      ),
+    );
+  }
+}
+
+class BottomBar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(
+                Icons.book,
+                color: Colors.white,
+            ),
+            Icon(
+              Icons.add_circle,
+              color: Colors.white,
+              size: 50,
+            ),
+            Icon(
+              Icons.whatshot,
+              color: Colors.white,
+            ),
+          ],
+        ),
+        height: 50.0,
+        color: Color(0xFF00b894));
+  }
 }
